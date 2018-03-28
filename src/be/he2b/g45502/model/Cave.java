276fd62e99f68b71a7542daa40db2ff18388a29f
Square04299 <christopher.bourrel0429@gmail.com>
2018-03-28 12:29:41 +0200

    v 0.1 End of Itération 1

--- src/be/he2b/g45502/model/Cave.java
+++ src/be/he2b/g45502/model/Cave.java
@@ -23,15 +23,6 @@ public class Cave {
     }
 
     /**
-     * Set a new treasure with a specified amount
-     *
-     * @param lastDiscoveredTreasure
-     */
-    public void setLastDiscoveredTreasure(Treasure lastDiscoveredTreasure) {
-        this.lastDiscoveredTreasure = lastDiscoveredTreasure;
-    }
-
-    /**
      * Will create a new room for the (explorers)
      *
      * @param explorers list of player
@@ -40,9 +31,8 @@ public class Cave {
         if (explorers.isEmpty()) {
             return;
         }
-        Treasure newTreasure = new Treasure();
-        newTreasure.explore(explorers);
-        setLastDiscoveredTreasure(newTreasure);
+        this.lastDiscoveredTreasure = new Treasure();
+        lastDiscoveredTreasure.explore(explorers);
     }
 
 }
